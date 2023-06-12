02a53d49bd7a074f3c1f8f77c5b18a73bd58d75c
IHateBard <kulon1236@gmail.com>
2023-06-12 18:00:06 +0300

    1

--- backend/app/Http/Controllers/InformationController.php
+++ backend/app/Http/Controllers/InformationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Information;
+use Illuminate\Support\Facades\Validator;
 
 class InformationController extends Controller
 {
@@ -12,35 +13,39 @@ class InformationController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->validate([
+
+        $validator = Validator::make($request->all(), [
             'group_name' => 'required',
             'group_number' => 'required',
             'sender_name' => 'required',
             'message_topic' => 'required',
             'message_text' => 'required',
-            'file' => 'nullable|file',
+            //'file' => 'nullable|text',
+            //'ext' => 'text',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), Response::HTTP_BAD_REQUEST);
+        }
 
         // Создаем новую запись информации
         $information = Information::create([
-            'group_name' => $data['group_name'],
-            'group_number' => $data['group_number'],
-            'sender_name' => $data['sender_name'],
-            'message_topic' => $data['message_topic'],
-            'message_text' => $data['message_text'],
+            'group_name' => $request->input('group_name'),
+            'group_number' => $request->input('group_number'),
+            'sender_name' => $request->input('sender_name'),
+            'message_topic' => $request->input('message_topic'),
+            'message_text' => $request->input('message_text'),
         ]);
 
         // Если файл загружен, сохраняем его и обновляем путь к файлу в записи информации
-        if ($request->hasFile('file')) {
-            $file = $request->file('file');
-            $file_path = $file->store('files');
+        if ($request->input('file')) {
+            $file = base64_decode($request->input('file'));
+            $file_path = uniqid() . '.' . $request->input('ext');
+            file_put_contents(public_path('/uploads/').$file_path, $file);
             $information->file_path = $file_path;
             $information->save();
         }
 
-        $response = new Response(['message' => 'Информация успешно добавлена'], 200);
-
-
-        return $response;
+        return response()->json(['message' => 'Информация успешно добавлена'], 200);
     }
 }
