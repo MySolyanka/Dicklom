231245ea69434f636f94981d85c39f9663b85124
Miva-Sun <sneofit11@gmail.com>
2023-06-12 18:01:07 +0300

    Для form_data

--- backend/app/Http/Controllers/InformationController.php
+++ backend/app/Http/Controllers/InformationController.php
@@ -28,23 +28,56 @@ class InformationController extends Controller
             return response()->json($validator->messages(), Response::HTTP_BAD_REQUEST);
         }
 
-        // Создаем новую запись информации
-        $information = Information::create([
-            'group_name' => $request->input('group_name'),
-            'group_number' => $request->input('group_number'),
-            'sender_name' => $request->input('sender_name'),
-            'message_topic' => $request->input('message_topic'),
-            'message_text' => $request->input('message_text'),
-        ]);
+        $data = $request->all();
 
-        // Если файл загружен, сохраняем его и обновляем путь к файлу в записи информации
-        if ($request->input('file')) {
-            $file = base64_decode($request->input('file'));
-            $file_path = uniqid() . '.' . $request->input('ext');
-            file_put_contents(public_path('/uploads/').$file_path, $file);
-            $information->file_path = $file_path;
-            $information->save();
+
+        $model = new information(); // Создание новой модели
+        $model->field1 = $data['group_name']; // Замените "field1" на имя поля в вашей таблице
+        $model->field2 = $data['group_number'];
+        $model->field2 = $data['sender_name'];
+        $model->field2 = $data['message_topic'];
+        $model->field2 = $data['message_text'];
+
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+
+            // Генерация уникального имени файла
+            $fileName = time() . '_' . $file->getClientOriginalName();
+
+            // Сохранение файла в указанной папке с уникальным именем
+            $filePath = $file->storeAs('/uploads/', $fileName); // Замените "your_directory" на путь к папке, в которую вы хотите сохранить файл
+
+            $model->file_path = $filePath;
         }
+
+
+        $model->save();
+
+
+
+
+
+
+
+
+        // Создаем новую запись информации
+//        $information = Information::create([
+//            'group_name' => $request->input('group_name'),
+//            'group_number' => $request->input('group_number'),
+//            'sender_name' => $request->input('sender_name'),
+//            'message_topic' => $request->input('message_topic'),
+//            'message_text' => $request->input('message_text'),
+//        ]);
+//
+//        // Если файл загружен, сохраняем его и обновляем путь к файлу в записи информации
+//        if ($request->input('file')) {
+//            $file = base64_decode($request->input('file'));
+//            $file_path = uniqid() . '.' . $request->input('ext');
+//            file_put_contents(public_path('/uploads/').$file_path, $file);
+//            $information->file_path = $file_path;
+//            $information->save();
+//        }
 
         return response()->json(['message' => 'Информация успешно добавлена'], 200);
     }
